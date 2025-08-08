d1bedd644f691bb42fcf70b1ff4cdca89c6aed5c
MaxDubovik <maks.dubovik.98@gmail.com>
2025-08-08 17:09:27 +0300

    fix bags

--- todo-app/js/app.js
+++ todo-app/js/app.js
@@ -5,8 +5,9 @@ const taskList = document.getElementById('taskList');
 addTaskBtn.addEventListener('click', addTask);
 
 function addTask() {
-  const taskText = taskInput.ariaValueMax.trim();
-  if (taskInput === '') return;
+
+  const taskText = taskInput.value.trim();
+  if (taskText === '') return;
 
   const li = document.createElement('li');
   li.textContent = taskText;
