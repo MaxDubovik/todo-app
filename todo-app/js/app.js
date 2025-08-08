3481ba7298aa4c1771cab0eca5c8baecfe5fdae7
MaxDubovik <maks.dubovik.98@gmail.com>
2025-08-08 17:28:39 +0300

    add localstorage

--- todo-app/js/app.js
+++ todo-app/js/app.js
@@ -2,27 +2,71 @@ const taskInput = document.getElementById('taskInput');
 const addTaskBtn = document.getElementById('addTaskBtn');
 const taskList = document.getElementById('taskList');
 
+let tasks = [];
+
+// загружаем задачи при старте
+loadTasks();
+
 addTaskBtn.addEventListener('click', addTask);
+taskInput.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') addTask();
+})
 
 function addTask() {
 
   const taskText = taskInput.value.trim();
   if (taskText === '') return;
 
-  const li = document.createElement('li');
-  li.textContent = taskText;
+  const newTask = {
+    text: taskText,
+    completed: false
+  }
 
-  // удаление по клику
-  li.addEventListener('click', () => {
-    li.classList.toggle('completed');
-  });
+  tasks.push(newTask);
+  saveTasks();
+  renderTasks();
 
-  // ПКМ - удаление
-  li.addEventListener('contextmenu', (e) => {
-    e.preventDefault;
-    li.remove();
-  });
-
-  taskList.appendChild(li);
   taskInput.value = '';
+}
+
+function renderTasks() {
+  taskList.innerHTML = '';
+
+  tasks.forEach((task, index) => {
+    const li = document.createElement('li');
+    li.textContent = task.text;
+
+    if (task.completed) {
+      li.classList.add('completed');
+    }
+
+    // отмена выполнения
+    li.addEventListener('click', () => {
+      tasks[index].completed = !tasks[index].completed;
+      saveTasks();
+      renderTasks();
+    })
+
+    // удаление по ПКМ
+    li.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+      tasks.splice(index, 1);
+      saveTasks();
+      renderTasks();
+    })
+
+    taskList.appendChild(li);
+  })
+}
+
+function saveTasks() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasks() {
+  const saved = localStorage.getItem('tasks');
+  if (saved) {
+    tasks = JSON.parse(saved);
+    renderTasks();
+  }
 }
